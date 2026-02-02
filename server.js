58eb590c5128a84bc9d22170df46388337658b1e
Teenu2005 <teenuanandanand@gmail.com>
2026-02-02 21:40:02 +0530

    autmation set up

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
 const http = require("http");
 const next = require("next");
-const fs = require("fs");
-
-const log = (msg) => {
-  try {
-    fs.appendFileSync(
-      __dirname + "/logs/app.log",
-      `[${new Date().toISOString()}] ${msg}\n`
-    );
-  } catch (e) {}
-};
-
-process.on("uncaughtException", (err) => {
-  log("UNCAUGHT EXCEPTION: " + err.stack);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (reason) => {
-  log("UNHANDLED PROMISE: " + reason);
-});
-
-log("Starting Next.js server...");
 
 const app = next({
   dev: false,
@@ -29,15 +8,12 @@ const app = next({
 
 const handle = app.getRequestHandler();
 
-app.prepare()
-  .then(() => {
-    http.createServer((req, res) => {
+app.prepare().then(() => {
+  http
+    .createServer((req, res) => {
       handle(req, res);
-    }).listen(process.env.PORT, () => {
-      log("Server listening on port " + process.env.PORT);
+    })
+    .listen(process.env.PORT, () => {
+      // Server started
     });
-  })
-  .catch((err) => {
-    log("NEXT PREPARE FAILED: " + err.stack);
-    process.exit(1);
-  });
+});
